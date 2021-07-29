9ca6046295d1af52a0d006efab6f305369ed23b2
prince-deb <78799922+prince-deb@users.noreply.github.com>
2021-07-29 21:23:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+moustache=loadImage(' https://i.postimg.cc/3x3QzSGq/m.png');
 }
 function setup(){
     canvas=createCanvas(300,300);
@@ -9,21 +11,25 @@ function setup(){
     video.hide();
     poseNet=ml5.poseNet(video,modelLoaded);
     poseNet.on('pose',gotPoses);
+
 }
 function draw(){
- image(video,0,0,300,300);
+image(video,0,0,300,300);
+image(moustache,noseX,noseY,30,30);
 }
 function take_snapshot(){
     save("Filtered image😁.png");
 }
 function modelLoaded(){
-    console.log('loaded');
+    console.log('LOADED');
 }
 function gotPoses(results){
-    if(results.length>0)
+    if (results.length>0)
     {
         console.log(results);
-        console.log("nose x ="+ results[0].pose.nose.x);
-        console.log("nose y ="+results[0].pose.nose.y)
+        noseX =results[0].pose.nose.x;
+        noseY =results[0].pose.nose.y;
+        console.log("nose x="+noseX);
+        console.log("nose y="+noseY);
     }
 }
